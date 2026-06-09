14d1f522f1ff067470ab1e42c6d1f091378ea785
DecoDG <127780233+AndreLuizDG@users.noreply.github.com>
2026-06-09 19:25:43 +0000

    add  and  files

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,4 @@
+// Teste de um get na API de status para ver se volta 200
 test("GET to api/v1/status deve retornar 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
