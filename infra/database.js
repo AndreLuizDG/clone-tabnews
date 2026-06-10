bd41a8d0a1c7cd1b5976cbb6a60167ddab6965b7
DecoDG <127780233+AndreLuizDG@users.noreply.github.com>
2026-06-10 18:15:18 +0000

    add

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,7 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    databases: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
